31b896ead777a64ebcc7822b7755cb5d99b2fe73
Rick Silliker <ricksilliker@gmail.com>
2022-02-22 20:25:12 -0600

    first commit

---
+++ src/dragonfly2/rigs.py
@@ -0,0 +1,56 @@
+from abc import ABCMeta, abstractmethod
+import logging
+
+
+LOG = logging.getLogger()
+
+
+class BuildBreakpoint(object):
+    pass
+
+
+class AbstractRig(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self._build_steps = []
+
+    @property
+    def build_steps(self):
+        return self._build_steps
+
+    @abstractmethod
+    def register(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def initialize(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def build(self):
+        while True:
+            try:
+                job = self._build_queue.pop(0)
+                if isinstance(job, BuildBreakpoint):
+                    LOG.info('Breakpoint found, pausing build.')
+                    return
+                else:
+                    job['callback']()
+            except IndexError:
+                LOG.info('All jobs completed.')
+                return
+            except:
+                LOG.exception('Something went wrong during rig build.')
+                return
+
+
+class RigTask(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.logger = logging.getLogger()
+
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError
